6ae30d57abf5e3d6919c6e73efd1e51080ec1cb3
zmj1316 <zmj1316@gmail.com>
2015-07-20 08:12:32 +0800

    Signed-off-by: zmj1316 <zmj1316@gmail.com>

---
+++ p3/1/shell1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# File Name:	shell1.sh
+# Author:	Key Zhang
+# Written:	7-17-2015
+# Modified:	
+# Purpose:	To count the number of normal directory and excuteble files, 
+# and the number of bytes under given directory which is passed in through argument.
+# Brief Description:	
